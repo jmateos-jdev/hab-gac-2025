e2cee2028782242b4e59f445b5679a986035d0e2
unknown <joaquin.mateos@jdev.com.ar>
2025-04-21 11:54:33 -0300

    agregamos punto

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           Contador: {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR.
         </p>
       </div>
       <p className="read-the-docs">
