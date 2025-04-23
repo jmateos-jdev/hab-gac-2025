b52638e36b354ee4dc082a2d8a2262912094223c
unknown <joaquin.mateos@jdev.com.ar>
2025-04-23 12:51:03 -0300

    Automatización de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           Contador: {count}
         </button>
-        <p>RAMA AAI-74</p>
+        <p>Despliegue Automático en Vercel y Netlify</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
